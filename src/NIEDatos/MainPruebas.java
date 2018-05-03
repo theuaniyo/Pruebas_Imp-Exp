4cb85f36e62da96a42056a2bc688cff127459c49
theuaniyo <theuaniyo@theuaniyo-System>
2018-05-03 20:06:05 +0200

    -Creación paquete NIEDatos

---
+++ src/NIEDatos/MainPruebas.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package NIEDatos;
+
+import administradorDeTareas.TareaAgenda;
+import administradorDeTareas.TareaEntrada;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Juanjo Luque
+ */
+public class MainPruebas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        ArrayList<String> tareaEntrada = AnalizadorClase.leerClase(TareaEntrada.class);
+        
+        for (String str : tareaEntrada) {
+            System.out.println(str);
+        }
+        
+        ArrayList<String> tareaAgenda = AnalizadorClase.leerClase(TareaAgenda.class);
+        
+        for (String str : tareaAgenda) {
+            System.out.println(str);
+        }
+        
+        for (String str : AnalizadorClase.clasesDelPaquete());
+        
+        System.out.println(AnalizadorClase.clasePadre(TareaAgenda.class).getName());
+
+    }
+
+}
